3f7478a03203889f206775f4afc805d3e234a003
CalebDuarte <caleb.jw.0309@gmail.com>
2025-10-21 15:42:42 -0700

    Delete Day-5/hour-convert.js

--- Day-5/hour-convert.js
+++
@@ -1,8 +0,0 @@
-// Write a js program to convert hours to minutes and seconds
-let prompt = require("prompt-sync")();
-let hour = parseInt(prompt(" Enter the number of hours you want to convert "));
-let sec = 3600 * hour;
-let min = 60 * hour;
-
-
- console.log(`Your converted time is ${hour} hours = ${min}mins = ${sec}seconds`) 
